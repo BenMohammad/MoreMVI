c3f7b65e80602181585e4a320334c6045a5a305b
Ben Mohammad <benmohammad593@gmail.com>
2021-03-28 11:51:44 +0100

    complete.......

--- app/src/main/java/com/benmohammad/moremvi/presentation/home/ui/HomeActivity.kt
+++ app/src/main/java/com/benmohammad/moremvi/presentation/home/ui/HomeActivity.kt
@@ -1,10 +1,16 @@
 package com.benmohammad.moremvi.presentation.home.ui
 
+import androidx.core.view.isVisible
+import androidx.core.widget.doOnTextChanged
+import com.benmohammad.moremvi.R
 import com.benmohammad.moremvi.presentation.common.BaseActivity
+import com.benmohammad.moremvi.presentation.common.getMessage
+import com.benmohammad.moremvi.presentation.common.runIfTrue
 import com.benmohammad.moremvi.presentation.home.HomeAction
 import com.benmohammad.moremvi.presentation.home.HomeIntent
 import com.benmohammad.moremvi.presentation.home.HomeState
 import com.benmohammad.moremvi.presentation.home.HomeViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity
     : BaseActivity<HomeIntent, HomeAction, HomeState, HomeViewModel>(HomeViewModel::class.java){
@@ -13,22 +19,49 @@ class HomeActivity
 
 
     override fun render(state: HomeState) {
-        TODO("Not yet implemented")
+        homeProgress.isVisible = state is HomeState.Loading
+        homeProgress.isVisible = state is HomeState.Exception
+        homeListCharacters.isVisible =
+                state is HomeState.ResultSearch || state is HomeState.ResultAllPersona
+
+        when (state) {
+            is HomeState.ResultAllPersona -> {
+                adapter.updateList(state.data)
+            }
+            is HomeState.ResultSearch -> {
+                adapter.updateList(state.data)
+            }
+
+            is HomeState.Exception -> {
+                homeMessage.text = state.callErrors.getMessage(this)
+            }
+        }
     }
 
     override fun getLayoutResId(): Int {
-        TODO("Not yet implemented")
+        return R.layout.activity_main
     }
 
     override fun initUI() {
-        TODO("Not yet implemented")
+        homeListCharacters.adapter = adapter
     }
 
     override fun initDATA() {
-        TODO("Not yet implemented")
+        dispatchIntent(HomeIntent.LoadAllCharacters)
     }
 
     override fun initEVENT() {
-        TODO("Not yet implemented")
+        homeSearchImage.setOnClickListener {
+            homeSearchText.text.isNullOrBlank().runIfTrue {
+                dispatchIntent(HomeIntent.SearchCharacter(homeSearchText.text.toString()))
+            }
+        }
+        homeSearchText.doOnTextChanged { text, start, before, count ->
+            text.isNullOrBlank()
+                    .and(mState is HomeState.ResultSearch)
+                    .runIfTrue {
+                        dispatchIntent(HomeIntent.ClearSearch)
+                    }
+        }
     }
 }
